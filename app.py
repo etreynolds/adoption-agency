27628f9e679f5720fd65f8e5af584a5ec4b11e34
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2023-01-24 23:55:38 -0700

    finish agency

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, render_template, redirect, flask
+from flask import Flask, request, render_template, redirect, flash, url_for
 from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Pet
+from forms import AddPetForm, EditPetForm
 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///employees_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "keepitsecret"
@@ -16,6 +18,43 @@ connect_db(app)
 
 @app.route('/')
 def home_page():
-    """Show homepage"""
+    """Show homepage."""
 
-    return render_template("home.html")
+    pets = Pet.query.all()
+    return render_template("home.html", pets=pets)
+
+
+@app.route('/add', methods=["GET", "POST"])
+def add_pet():
+    """Add a pet."""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        data = {k: v for k, v in form.data.items() if k != "csrf_token"}
+        new_pet = Pet(name=form.name.data, age=form.age.data,
+                      species=form.species.data, photo_url=form.photo_url.data, notes=form.notes.data)
+        db.session.add(new_pet)
+        db.session.commit()
+        flash(f"{new_pet.name} added.")
+        return redirect("/")
+
+    else:
+        return render_template("pet_add_form.html", form=form)
+
+
+@app.route('/<int:pet_id>', methods=["GET", "POST"])
+def edit_pet(pet_id):
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        pet.photo_url = form.photo_url.data
+        db.session.commit()
+        flash(f"{pet.name} updated.")
+        return redirect('/')
+
+    else:
+        return render_template("pet_edit_form.html", form=form, pet=pet)
--- flask-adopt-solution/part-1/seed.sql
+++ flask-adopt-solution/part-1/seed.sql
@@ -19,7 +19,7 @@ CREATE TABLE pets
 INSERT INTO pets
   (name, species, photo_url, age, notes, available)
 VALUES
-  ('Woofly', 'dog', 'https://www.what-dog.net/Images/faces2/scroll001.jpg', 3, 'Incredibly adorable.', 't'),
+  ('Woofly', 'dog', 'https://media.4-paws.org/0/c/7/e/0c7e157aabf6ef2f316c05607813b130c994f293/VIER%20PFOTEN_2019-03-15_001-2886x1999.jpg', 3, 'Incredibly adorable.', 't'),
   ('Porchetta', 'porcupine', 'http://kids.sandiegozoo.org/sites/default/files/2017-12/porcupine-incisors.jpg', 4, 'Somewhat spiky!', 't'),
   ('Snargle', 'cat', 'https://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg', null, null, 't'),
   ('Dr. Claw', 'cat', null, null, null, 't');
